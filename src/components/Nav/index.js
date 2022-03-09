91cba5bc0ed83112b2dba931f333b8f2aa8864de
lucaspalleros <lucaspalleros@gmail.com>
2022-03-09 13:11:38 -0300

    fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {AiOutlineMenu} from 'react-icons/ai';
+// import {AiOutlineMenu} from 'react-icons/ai';
+import "../Nav/Nav.css";
 
 const Nav = () => {
   return(
     <div className='nav-container'>
         <nav className='nav-content'>
-            <div className='nav-title'><AiOutlineMenu/></div>
+            {/* <div className='nav-title'><AiOutlineMenu/></div> */}
+            <div className='nav-title'>Title</div>
             <div className='nav-search'>
               <input type="text" className="nav-input" placeholder="Search"/>
             </div>
