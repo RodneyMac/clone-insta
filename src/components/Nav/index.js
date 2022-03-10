0e98ad2524bb7445f0a088b8ac88bcff3b355bfb
RodneyMac <rodneydavidmaciel@gmail.com>
2022-03-10 15:28:56 -0300

    updated nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
-// import {AiOutlineMenu} from 'react-icons/ai';
+import instaLogo from "./img/insta-logo.png";
+import {VscHome} from 'react-icons/vsc';
+import {TiLocationArrowOutline} from "react-icons/ti";
+import {FiPlusSquare} from "react-icons/fi";
+import {MdExplore} from "react-icons/md";
+import {AiOutlineHeart} from "react-icons/ai";
+import {BiSearch} from "react-icons/bi";
+// import imgPerfil from "./img/img-perfil.jpg";
 import "../Nav/Nav.css";
 
 const Nav = () => {
   return(
     <div className='nav-container'>
         <nav className='nav-content'>
-            {/* <div className='nav-title'><AiOutlineMenu/></div> */}
-            <div className='nav-title'>Title</div>
+            <div className='nav-logo'><img src={instaLogo} alt="instaLogo" className='img-logo'/></div>
             <div className='nav-search'>
-              <input type="text" className="nav-input" placeholder="Search"/>
+              <BiSearch className='search-icon'/>
+              <input type="text" className="nav-input" placeholder="Busca"/>
             </div>
-            <div className='nav-icons'>Icons</div>
+            <div className='nav-icons'>
+                <div className='div-icon'><VscHome className="n-i"/></div>
+                <div className='div-icon'><TiLocationArrowOutline className="n-i"/></div>
+                <div className='div-icon'><FiPlusSquare className="n-i"/></div>
+                <div className='div-icon'><MdExplore className="n-i"/></div>
+                <div className='div-icon'><AiOutlineHeart className="n-i"/></div>
+                <div className='div-user-circle'>
+                  <div className='user-circle div-icon n-i'></div>
+                </div>
+            </div>
         </nav>
+        {/* <hr/> */}
     </div>
   )
 }
