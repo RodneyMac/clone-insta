7dfee236888ea5a2ea5c18d738123f614a7b0abf
RodneyMac <rodneydavidmaciel@gmail.com>
2022-03-11 18:44:11 -0300

    updated followings component, modal, header, options, icon

--- src/components/Following/index.js
+++ src/components/Following/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "../Following/Following.css";
+import {GrClose} from "react-icons/gr";
 
 const Following = () => {
   return (
-    <div>
-        <h2>Following Modal</h2>
+    <div className='modal-container'>
+        <div className='modal-header'><h2 className='modal-title'>Siguiendo</h2><div className='div-icon-exit'><GrClose className='icon-exit'/></div></div>
+        <div className='modal-items'>
+          <div className='modal-items-personas'>Personas</div>
+          <div className='modal-items-hashtags'>Hashtags</div>
+        </div>
     </div>
   )
 }
