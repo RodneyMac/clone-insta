717b5246d923655aa620b793ab1b9ca0b07f2d5c
RodneyMac <rodneydavidmaciel@gmail.com>
2022-03-11 17:34:54 -0300

    user profile image fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,10 +6,13 @@ import {FiPlusSquare} from "react-icons/fi";
 import {MdExplore} from "react-icons/md";
 import {AiOutlineHeart} from "react-icons/ai";
 import {BiSearch} from "react-icons/bi";
-import imgPerfil from "./img/user-picture.jpg";
 import "../Nav/Nav.css";
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
+
+  const user = useSelector((state) => state.user);
+
   return(
     <div className='nav-container'>
         <nav className='nav-content'>
@@ -24,11 +27,10 @@ const Nav = () => {
                 <div className='div-icon'><FiPlusSquare className="n-i"/></div>
                 <div className='div-icon'><MdExplore className="n-i"/></div>
                 <div className='div-icon'><AiOutlineHeart className="n-i"/></div>
-                <div className='user-circle div-icon n-i'><img src={imgPerfil} alt="img-logo-nav" className='img-nav-logo'/></div>
-                {/* <div className='user-circle div-icon n-i'>{props.user.img}</div> */}
+                <div className='user-circle div-icon n-i'><img src={user.img} alt="img-logo-nav" className='img-nav-logo'/></div>
+                
             </div>
         </nav>
-        {/* <hr/> */}
     </div>
   )
 }
