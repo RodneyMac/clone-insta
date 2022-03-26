aac8f18651d4348aae550463ebc2a6bd7e76e45d
RodneyMac <rodneydavidmaciel@gmail.com>
2022-03-26 16:51:51 -0300

    delete function added to followers modal

--- src/components/Followers/index.js
+++ src/components/Followers/index.js
@@ -1,60 +1,41 @@
 import React from 'react';
 import "../Followers/Followers.css";
 import {GrClose} from "react-icons/gr";
-import image1 from "./img/img-1.png";
-import image2 from "./img/img-2.png";
-import image3 from "./img/img-3.png";
-import image4 from "./img/img-4.png";
 import {closeModal} from "../../data/common/commonSlice";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {deleteFollower} from "../../data/user/userSlice";
 
-const seguidores = [
-  {
-    id: 1,
-    username: "johnnyrotten",
-    name: "John",
-    img: image1,
-  },
-  {
-    id: 2,
-    username: "moonkate",
-    name: "Kate",
-    img: image2,
-  },
-  {
-    id: 3,
-    username: "paul_90",
-    name: "Paul",
-    img: image3,
-  },
-  {
-    id: 4,
-    username: "onlynani",
-    name: "Hannah",
-    img: image4,
-  }
-]
 
 const FollowersCard = (props) => {
+
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    const userID = props.data.id;
+    dispatch(deleteFollower({id: userID}));
+  }
+
   return(
     <div className='followers-modal-content'>
-      <div className='f-modal-user-img'><img src={props.data.img} alt="image-3" className='f-modal-user-image'/></div>
+      <div className='f-modal-user-img'><img src={props.data.img} alt="user-image" className='f-modal-user-image'/></div>
         <div className='f-modal-user-data'>
           <div className='f-modal-user-data-username'>{props.data.username}</div>
           <div className='f-modal-user-data-name'>{props.data.name}</div>
         </div>
-        <div className='div-f-modal-btn'><button className='f-modal-btn-eliminar'>Eliminar</button></div>
+        <div className='div-f-modal-btn'><button className='f-modal-btn-eliminar' onClick={handleDelete}>Eliminar</button></div>
     </div>
   )
 }
 
 const Followers = () => {
 
-const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
-const handleCloseModalFollowers = () => {
-  dispatch(closeModal());
-}
+  const dispatch = useDispatch();
+
+  const handleCloseModalFollowers = () => {
+    dispatch(closeModal());
+  }
 
   return (
     <div className='followers-modal-container'>
@@ -64,7 +45,7 @@ const handleCloseModalFollowers = () => {
             <div className='div-modal-h-icon'><GrClose className='followers-icon-exit' onClick={handleCloseModalFollowers}/></div>
         </div>
         <div>
-          {seguidores.map(item => (
+          {user.followers.map(item => (
             <FollowersCard key={item.id} data={item}/>
           ))}
         </div>
--- src/data/user/userSlice.js
+++ src/data/user/userSlice.js
@@ -1,4 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import image1 from "../../components/Followers/img/img-1.png";
+import image2 from "../../components/Followers/img/img-2.png";
+import image3 from "../../components/Followers/img/img-3.png";
+import image4 from "../../components/Followers/img/img-4.png";
 
 export const userSlice = createSlice({
   name: 'user',
@@ -6,19 +10,50 @@ export const userSlice = createSlice({
     name: "user",
     email: "user@gmail.com",
     id: "845078",
-    followers: [],
+    followers: [
+      {
+        id: 1,
+        username: "johnnyrotten",
+        name: "John",
+        img: image1,
+      },
+      {
+        id: 2,
+        username: "moonkate",
+        name: "Kate",
+        img: image2,
+      },
+      {
+        id: 3,
+        username: "paul_90",
+        name: "Paul",
+        img: image3,
+      },
+      {
+        id: 4,
+        username: "onlynani",
+        name: "Hannah",
+        img: image4,
+      }
+    ],
     post: [],
     following: [],
     img: "https://illumesense.com/resources/illumesense/style/img/website/profile-picture-blanks/male-profile.jpg"
   },
   reducers: {
     changeName: (state) => {
-        state.name= "Lucas"
+        state.name= "Usuario"
+    },
+    deleteFollower: (state, action) => {
+      return ({
+        ...state,
+        followers: state.followers.filter((item) => item.id != action.payload.id)
+      })
     }
-},
+  },
 })
 
 // Action creators are generated for each case reducer function
-export const { changeName } = userSlice.actions
+export const { changeName, deleteFollower} = userSlice.actions
 
 export default userSlice.reducer
